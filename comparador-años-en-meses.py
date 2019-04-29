23e94d416b3a75f68f553500195fa31f70fbfa7b
JoseMariaGM <43352265+JoseMariaGM@users.noreply.github.com>
2019-04-29 23:12:49 +0200

    Create comparador-años-en-meses.py

---
+++ comparador-años-en-meses.py
@@ -0,0 +1,29 @@
+Comparador-años-en-meses.
+
+
+<pre>
+
+# coding : utf8
+# python 3
+diciembre = 12
+enero = 1
+resultado = diciembre-enero
+#Meses para llegar hasta Enero
+if enero <= diciembre :
+    print("Faltan" , resultado ,  "meses para llegar a la Navidad")
+else:
+    print("El número que ha utilizado es superior al número máximo de meses")
+    
+ 	
+</pre>
+
+
+### Tabla
+
+
+| Casos a probar | Resultado esperado |
+| -------------- | ------------------ |
+| Enero-Diciembre | Respuesta |
+| Enero-Diciembre | Falta 11 meses para poder llegar a Diciembre |
+| Enero-Enero  | Es el mismo mes, por favor, cambia el número utilizado |
+| Diciembre-Enero | Falta 1 mes per arribar al Gener de l'any que ve. |
