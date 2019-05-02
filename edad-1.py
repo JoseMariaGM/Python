a67a8c605126d43a53a581a344c1aec97d5f34c8
JoseMariaGM <43352265+JoseMariaGM@users.noreply.github.com>
2019-05-02 23:33:01 +0200

    Create edad-1.py

---
+++ edad-1.py
@@ -0,0 +1,26 @@
+<pre>
+
+# coding utf8
+# python 3
+
+anyos=int(input("¿Cuántos años tienes? :"))
+
+anyo_minimo = 15
+anyo_máximo = 17
+
+if (anyos<15):
+    print(" Eres menor que 15 años, no puedes pasar a la sesion de tarde")
+if (anyos>17):
+    print("Eres mayor que 17 años, no puedes pasar a la sesion de tarde")
+if (anyos == 15,17):
+    print("Tienes entre 15 y 17 años, puedes pasar a la sesión de tarde")
+
+</pre>
+
+
+| Casos a probar | Resultado esperado |
+| -------------- | ------------------ |
+| edades | Respuesta |
+| < 15| Eres más pequeño de lo mínimo, no puedes pasar |
+| 15 , 17  | Tienes la edad permitida, puedes pasar |
+| > 17  | Tienes más edad de lo permitido |
